bedd0f8fbdfb1039a15d97309d36842d72667611
Jimmy <aspire.dy@hotmail.com>
2021-04-29 10:14:13 -0500

    Nuevo atributo

--- src/main/java/Codigo1/User.java
+++ src/main/java/Codigo1/User.java
@@ -1,10 +1,11 @@
 package Codigo1;
 
 public class User {
-    private String name, familyname;
+    private String name, familyname, asdasd;
 
     public User(String name, String familyname) {
         this.name = name;
         this.familyname = familyname;
+
     }
 }
