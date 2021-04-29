3f60dde6991a1b46b9fc8b0512b25d3bb796820b
cesar <juegoskm@hotmail.com>
2021-04-29 10:26:42 -0500

    cambios1

--- src/main/java/Codigo1/User.java
+++ src/main/java/Codigo1/User.java
@@ -1,7 +1,7 @@
 package Codigo1;
 
 public class User {
-    private String name, familyname, asdasd;
+    private String name, familyname, asdasd  ;
 
     public User(String name, String familyname) {
         this.name = name;
