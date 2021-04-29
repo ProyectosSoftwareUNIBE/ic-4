71ea520fff67fb830f737334505dd421db858477
Jimmy <aspire.dy@hotmail.com>
2021-04-29 10:24:41 -0500

    Creacion de un test

--- src/main/java/Codigo1/User.java
+++ src/main/java/Codigo1/User.java
@@ -1,11 +1,33 @@
 package Codigo1;
 
 public class User {
-    private String name, familyname, asdasd,asdasda;
+    private String name, familyname;
 
     public User(String name, String familyname) {
         this.name = name;
         this.familyname = familyname;
 
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamilyname() {
+        return familyname;
+    }
+
+    public void setFamilyname(String familyname) {
+        this.familyname = familyname;
+    }
+
+    public String getNamemayus(){
+        return this.name.toUpperCase();
+
+    }
+
 }
