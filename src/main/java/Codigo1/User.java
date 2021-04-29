355a8c18e9742b6b216c17094925ac1f44823d8e
Jimmy <aspire.dy@hotmail.com>
2021-04-29 10:06:26 -0500

    Generar constructor

--- src/main/java/Codigo1/User.java
+++ src/main/java/Codigo1/User.java
@@ -2,4 +2,9 @@ package Codigo1;
 
 public class User {
     private String name, familyname;
+
+    public User(String name, String familyname) {
+        this.name = name;
+        this.familyname = familyname;
+    }
 }
